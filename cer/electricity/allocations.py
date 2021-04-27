d731c6b01c3a861986a2a71c4b6098e6e31e475b
Rowan Molony <rowan.molony@codema.ie>
2021-04-27 12:56:49 +0100

    Remove prefect from electricity allocations

--- cer/electricity/allocations.py
+++ cer/electricity/allocations.py
@@ -1,51 +1,15 @@
-import json
-from os import path
+from pathlib import Path
 from typing import Dict
 from typing import List
 
 import dask.dataframe as dd
 import numpy as np
 import pandas as pd
-from prefect import case
-from prefect import Flow
-from prefect import Parameter
-from prefect import task
-from prefect import mapped
 
-here = path.abspath(path.dirname(__file__))
-allocation_mappings_filepath = path.join(here, "allocation_mappings.json")
+HERE = Path(__file__).parent
 
 
-@task
-def get_path_to_allocations(dirpath: str) -> str:
-
-    return path.join(
-        dirpath,
-        "CER Electricity Revised March 2012",
-        "CER_Electricity_Documentation",
-        "SME and Residential allocations.xlsx",
-    )
-
-
-@task
-def read_allocations(filepath: str, usecols: List[str]) -> pd.DataFrame:
-
-    return pd.read_excel(
-        filepath,
-        engine="openpyxl",
-        usecols=usecols,
-    )
-
-
-@task
-def read_allocation_mappings(filepath: str) -> pd.DataFrame:
-
-    with open(filepath) as json_file:
-        return json.load(json_file)
-
-
-@task
-def decode_columns(
+def _decode_columns(
     df: pd.DataFrame,
     column_names: List[str],
     column_mappings: Dict[str, str],
@@ -59,22 +23,7 @@ def decode_columns(
     return df
 
 
-@task
-def write_to_parquet(df: pd.DataFrame, filepath: str) -> None:
-
-    df.to_parquet(filepath)
-
-
-@task
-def check_file_exists(filepath: str) -> bool:
-
-    return path.exists(filepath)
-
-
-with Flow("Map Allocations to Building IDs") as flow:
-
-    input_dirpath = Parameter("input_dirpath")
-    output_filepath = Parameter("output_filepath")
+def clean_allocations(input_dirpath, output_filepath):
 
     column_names = [
         "ID",
@@ -83,12 +32,18 @@ with Flow("Map Allocations to Building IDs") as flow:
         "Residential - stimulus allocation",
         "SME allocation",
     ]
-    path_to_allocations = get_path_to_allocations(input_dirpath)
+    allocations_raw = pd.read_excel(
+        Path(input_dirpath)
+        / "CER Electricity Revised March 2012"
+        / "CER_Electricity_Documentation"
+        / "SME and Residential allocations.xlsx",
+        engine="openpyxl",
+        usecols=column_names,
+    )
+    with open(HERE / "allocation_mappings.json") as json_file:
+        allocation_mappings = json.load(json_file)
 
-    allocations_raw = read_allocations(path_to_allocations, usecols=column_names)
-    allocation_mappings = read_allocation_mappings(allocation_mappings_filepath)
-
-    allocations_decoded = decode_columns(
+    allocations_decoded = _decode_columns(
         allocations_raw,
         column_names=[
             "Code",
@@ -98,5 +53,4 @@ with Flow("Map Allocations to Building IDs") as flow:
         ],
         column_mappings=allocation_mappings,
     )
-
-    write_to_parquet(allocations_decoded, output_filepath)
+    allocations_decoded.to_csv(output_filepath, index=False)
