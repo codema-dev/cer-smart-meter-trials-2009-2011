b8301e053a212267adfc5c6fe30ad911b503c7df
Rowan Molony <rowan.molony@codema.ie>
2021-04-27 12:56:07 +0100

    Remove prefect from electricity demand calc

--- cer/electricity/demand.py
+++ cer/electricity/demand.py
@@ -10,33 +10,21 @@ id      datetime                demand
 1392    2009-07-15 01:30:00     0.14
 """
 
-from glob import glob
-from os import path
+from pathlib import Path
 from typing import Iterable
 
 import dask.dataframe as dd
-from prefect import Flow
-from prefect import Parameter
-from prefect import task
+from dask.diagnostics import ProgressBar
 
 
-@task
-def get_path_to_raw_txt_files(dirpath: str) -> str:
+def _read_raw_txt_files(dirpath: Path) -> Iterable[dd.DataFrame]:
 
-    return path.join(
-        dirpath,
-        "CER Electricity Revised March 2012",
-    )
-
-
-@task
-def read_raw_txt_files(dirpath: str) -> Iterable[dd.DataFrame]:
-
-    filepaths = glob(f"{dirpath}/*.txt.zip")
+    filepaths = list(dirpath.glob("*.txt.zip"))
 
     return dd.read_csv(
         filepaths,
         compression="zip",
+        blocksize=None,
         header=None,
         sep=" ",
         names=["ID", "timeid", "demand"],
@@ -45,8 +33,7 @@ def read_raw_txt_files(dirpath: str) -> Iterable[dd.DataFrame]:
     )
 
 
-@task
-def slice_timeid_column(ddf: dd.DataFrame) -> dd.DataFrame:
+def _slice_timeid_column(ddf: dd.DataFrame) -> dd.DataFrame:
 
     ddf["day"] = ddf["timeid"].str.slice(0, 3).astype("int16")
     ddf["halfhourly_id"] = ddf["timeid"].str.slice(3, 5).astype("int8")
@@ -54,8 +41,7 @@ def slice_timeid_column(ddf: dd.DataFrame) -> dd.DataFrame:
     return ddf.drop(columns=["timeid"])
 
 
-@task
-def convert_dayid_to_datetime(ddf: dd.DataFrame) -> dd.DataFrame:
+def _convert_dayid_to_datetime(ddf: dd.DataFrame) -> dd.DataFrame:
 
     ddf["datetime"] = (
         dd.to_datetime(
@@ -69,21 +55,13 @@ def convert_dayid_to_datetime(ddf: dd.DataFrame) -> dd.DataFrame:
     return ddf.drop(columns=["day", "halfhourly_id"])
 
 
-@task
-def write_parquet(ddf: dd.DataFrame, savepath: str):
+def clean_electricity_demands(input_dirpath, output_dirpath):
 
-    return ddf.to_parquet(savepath)
+    demand_raw = _read_raw_txt_files(
+        Path(input_dirpath) / "CER Electricity Revised March 2012"
+    )
+    demand_with_times = _slice_timeid_column(demand_raw)
+    demand_with_datetimes = _convert_dayid_to_datetime(demand_with_times)
 
-
-with Flow("Clean Electricity Data") as flow:
-
-    input_dirpath = Parameter("input_dirpath")
-    output_dirpath = Parameter("output_dirpath")
-
-    path_to_raw_txt_files = get_path_to_raw_txt_files(input_dirpath)
-
-    demand_raw = read_raw_txt_files(path_to_raw_txt_files)
-    demand_with_times = slice_timeid_column(demand_raw)
-    demand_with_datetimes = convert_dayid_to_datetime(demand_with_times)
-    
-    write_parquet(demand_with_datetimes, output_dirpath)
+    with ProgressBar():
+        demand_with_datetimes.to_parquet(output_dirpath)
