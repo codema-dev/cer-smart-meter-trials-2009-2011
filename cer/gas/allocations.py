9cb51714c20334ae534099a0e6efca71037ff770
Rowan Molony <rowan.molony@codema.ie>
2021-04-27 13:49:28 +0100

    Remove prefect from gas allocation cleaning

--- cer/gas/allocations.py
+++ cer/gas/allocations.py
@@ -1,48 +1,14 @@
 import json
-from os import path
+from pathlib import Path
 from typing import Dict
 from typing import List
 
 import pandas as pd
-from prefect import Flow
-from prefect import Parameter
-from prefect import task
 
-here = path.abspath(path.dirname(__file__))
+HERE = Path(__file__).parent
 
 
-@task
-def get_path_to_allocations(dirpath: str) -> str:
-
-    return path.join(
-        dirpath,
-        "CER Gas Revised October 2012",
-        "CER_Gas_Documentation",
-        "Residential allocations.xls",
-    )
-
-
-@task
-def read_allocations(filepath: str, usecols: List[str]) -> pd.DataFrame:
-
-    return pd.read_excel(
-        filepath,
-        engine="xlrd",
-        usecols=usecols,
-    )
-
-
-@task
-def get_allocation_mappings() -> pd.DataFrame:
-
-    filepath = path.join(here, "allocation_mappings.json")
-
-    with open(filepath) as json_file:
-        return json.load(json_file)
-
-
-@task
-def decode_column(
+def _decode_column(
     df: pd.DataFrame,
     column_name: str,
     column_mappings: Dict[str, str],
@@ -55,36 +21,26 @@ def decode_column(
     return df
 
 
-@task
-def write_to_parquet(df: pd.DataFrame, filepath: str) -> None:
-
-    df.to_parquet(filepath)
-
-
-@task
-def check_file_exists(filepath: str) -> bool:
-
-    return path.exists(filepath)
-
-
-with Flow("Map Allocations to Building IDs") as flow:
-
-    input_dirpath = Parameter("input_dirpath")
-    output_filepath = Parameter("output_filepath")
+def clean_allocations(input_dirpath, output_filepath="gas_allocations.csv"):
 
     column_names = [
         "ID",
         "Allocation",
     ]
-    path_to_allocations = get_path_to_allocations(input_dirpath)
+    allocations_raw = pd.read_excel(
+        Path(input_dirpath)
+        / "CER Gas Revised October 2012"
+        / "CER_Gas_Documentation"
+        / "Residential allocations.xls",
+        engine="xlrd",
+        usecols=column_names,
+    )
+    with open(HERE / "allocation_mappings.json") as json_file:
+        allocation_mappings = json.load(json_file)
 
-    allocations_raw = read_allocations(path_to_allocations, usecols=column_names)
-    allocation_mappings = get_allocation_mappings()
-
-    allocations_decoded = decode_column(
+    allocations_decoded = _decode_column(
         allocations_raw,
         column_name="Allocation",
         column_mappings=allocation_mappings,
     )
-
-    write_to_parquet(allocations_decoded, output_filepath)
+    allocations_decoded.to_csv(output_filepath, index=False)
